6e9af730af9a9d79958542dc997f75dacecf6edd
Akash <aakasharya2011@gmail.com>
2023-07-08 11:37:35 +0530

    Utils Added with logger and exception

---
+++ app.py
@@ -0,0 +1,11 @@
+from cellSegmentation.logger import logging
+from cellSegmentation.exception import AppException
+import sys
+
+logging.info("Welcome to the Custom log")
+
+try:
+    a =4/"5" 
+
+except Exception as e:
+    raise AppException(e,sys)
--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
 project_name = "cellSegmentation"
@@ -34,6 +34,7 @@ list_of_files = [
     "setup.py",
 ]
 
+
 for filepath in list_of_files:
     filepath = Path(filepath)
 
@@ -41,12 +42,14 @@ for filepath in list_of_files:
 
     if filedir!="":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directories:{filedir}for the file{filename}")
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
 
-    if (not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
-        with open(filepath,'w') as f:
+    
+    if(not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filename}")
 
+    
     else:
         logging.info(f"{filename} is already created")
