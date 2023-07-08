783163cdcfbf9701223e0b5040a3cb188f69c1b0
Akash <aakasharya2011@gmail.com>
2023-07-08 10:01:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cellSegmentation",
+    version= "0.0.0",
+    author = "Akash Arya",
+    author_email = "akasharyaice@yahoo.com",
+    packages = find_packages(),
+    install_requires = []
+)
