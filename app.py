393d6f64f9507771972f86cbc31dc1bc99792400
Akash <aakasharya2011@gmail.com>
2023-07-09 08:09:18 +0530

    Data Ingestion Done

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from cellSegmentation.logger import logging
-from cellSegmentation.exception import AppException
-import sys
+import sys,os
+from cellSegmentation.pipeline.training_pipeline import TrainPipeline
 
-logging.info("Welcome to the Custom log")
-
-try:
-    a =4/"5" 
-
-except Exception as e:
-    raise AppException(e,sys)
+obj = TrainPipeline()
+obj.run_pipeline()
+print("Training Done")
